a0f8bffc6cb3e535ea8629acfc6c01506260d3e6
JKCrafted <78131640+JKCrafted@users.noreply.github.com>
2025-01-24 23:57:59 +1100

    Snake ending

--- Assets/Scripts/Snake/SnakeObject.cs
+++ Assets/Scripts/Snake/SnakeObject.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SnakeObject : MonoBehaviour
@@ -6,7 +9,9 @@ public class SnakeObject : MonoBehaviour
     public GameObject snakeEnd;
     private Rigidbody rb;
     private SpawnApple spawnApple;
+    bool running = true;
     [SerializeField] private float speed = 100f;
+    private int wallHits = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,33 +23,41 @@ public class SnakeObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) 
+        if (running)
         {
-            rb.AddForce(Vector3.forward * speed);
+            if (Input.GetKeyDown(KeyCode.W)) 
+            {
+                rb.AddForce(Vector3.forward * speed);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                rb.AddForce(Vector3.back * speed);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                rb.AddForce(Vector3.left * speed);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                rb.AddForce(Vector3.right * speed);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            rb.AddForce(Vector3.back * speed);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            rb.AddForce(Vector3.left * speed);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            rb.AddForce(Vector3.right * speed);
-        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name.Contains("Apple"))
+        if (running)
         {
-            GrowSnake(collision.gameObject);
-        }
-        else if (collision.gameObject.name.Contains("Wall") || collision.gameObject.name.Contains("Snake"))
-        {
-            GameEnd();
+
+            if (collision.gameObject.name.Contains("Apple"))
+            {
+                GrowSnake(collision.gameObject);
+            }
+            else if (collision.gameObject.name.Contains("Wall") || (collision.gameObject.name.Contains("Snake") && collision.gameObject != gameManager.snake[1]))
+            {
+                GameEnd();
+            }
         }
     }
 
@@ -59,6 +72,31 @@ public class SnakeObject : MonoBehaviour
     }
     private void GameEnd()
     {
+        if (wallHits > 20)
+        {
+            running = false;
+            for (int i = 1; i < gameManager.snake.Count; i++) 
+            {
+                Destroy(gameManager.snake[i].GetComponent<SnakeEnd>());
+            }
 
+            for (int i = 0; i < gameManager.snake.Count; i++)
+            {
+                StartCoroutine(DestroySnake((gameManager.snake.Count - 1 - i), gameManager.snake[i]));
+    //            Destroy(gameManager.snake[i].GetComponent<SnakeEnd>());
+            }
+        }
+        else
+        {
+            wallHits += 1;
+        }
+
+
+        
+    }
+    private IEnumerator DestroySnake(int time, GameObject item)
+    {
+        yield return new WaitForSeconds(2f*time);
+        Destroy(item);
     }
 }
