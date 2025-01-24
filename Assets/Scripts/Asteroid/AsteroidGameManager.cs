bf12bffd62f6d31a78cb7eb909897528ec4f5b05
dawurth <111714223+dawurth@users.noreply.github.com>
2025-01-25 05:58:35 +1100

    score shows for user
    did import textmeshpro

--- Assets/Scripts/Asteroid/AsteroidGameManager.cs
+++ Assets/Scripts/Asteroid/AsteroidGameManager.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class AsteroidGameManager : MonoBehaviour
 {
     public int player_score;
+    public GameObject score_ui;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        updateScore(0); 
     }
 
     // Update is called once per frame
@@ -19,5 +20,8 @@ public class AsteroidGameManager : MonoBehaviour
     public void updateScore(int score)
     {
         player_score += score;
+        // update ui textmesh pro
+
+        score_ui.GetComponent<TMPro.TextMeshProUGUI>().text = "Score: " + player_score;
     }
 }
