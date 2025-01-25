4261d74e971ebbf9f354c7d3c774ed555ce14ea0
Aam66 <s3950302@student.rmit.edu.au>
2025-01-26 10:37:29 +1100

    flappy win condition adjustment

--- Assets/Scripts/Flappy/FlappyGameManager.cs
+++ Assets/Scripts/Flappy/FlappyGameManager.cs
@@ -9,7 +9,7 @@ public class FlappyGameManager : MonoBehaviour
     [SerializeField] private GameObject scoreUI;
     public float pillarSpeed = 1f;
     private EndState endState;
-    public int passingScore = 10;
+    public int passingScore = 50;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
