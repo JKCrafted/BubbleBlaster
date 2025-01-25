d945ea068037eea39a3e25fc235b7db634f69070
Aam66 <s3950302@student.rmit.edu.au>
2025-01-25 21:14:03 +1100

    changed pillar structure for ease in spike asset

--- Assets/Scripts/Flappy/PillarController.cs
+++ Assets/Scripts/Flappy/PillarController.cs
@@ -33,16 +33,58 @@ namespace Flappy
         private void Randomise()
         {
             int randomIndex = Random.Range(1, 8); // Randomly pick a block between 1 and 8
+            Debug.Log(randomIndex);
 
-            Transform block1 = transform.GetChild(randomIndex);
-            Transform block2 = transform.GetChild(randomIndex + 1);
-            Transform block3 = transform.GetChild(randomIndex + 2);
+            float emptySpacePosition = 0f;
+            if (randomIndex == 1)
+            {
+                emptySpacePosition = 0.45f;
+            }
+            else if (randomIndex == 2)
+            {
+                emptySpacePosition = 0.35f;
+            }
+            else if (randomIndex == 3)
+            {
+                emptySpacePosition = 0.25f;
+            }
+            else if (randomIndex == 4)
+            {
+                emptySpacePosition = 0.15f;
+            }
+            else if (randomIndex == 5)
+            {
+                emptySpacePosition = 0.05f;
+            }
+            else if (randomIndex == 6)
+            {
+                emptySpacePosition = -0.05f;
+            }
+            else if (randomIndex == 7)
+            {
+                emptySpacePosition = -0.15f;
+            }
+            else if (randomIndex == 8)
+            {
+                emptySpacePosition = -0.25f;
+            }
+
+            // Transform block1 = transform.GetChild(randomIndex);
+            // Transform block2 = transform.GetChild(randomIndex + 1);
+            // Transform block3 = transform.GetChild(randomIndex + 2);
+            Transform block1 = transform.GetChild(1);
+            Transform block2 = transform.GetChild(2);
 
             if (block1 != null && block2 != null)
             {
-                block1.gameObject.SetActive(false);
-                block2.gameObject.SetActive(false);
-                block3.gameObject.SetActive(false);
+                // float emptySpacePosition = (randomIndex - 3.5f)* 0.1f;
+
+                block1.localPosition = new Vector3(block1.localPosition.x, 0.5f + emptySpacePosition, block1.localPosition.z);
+                block2.localPosition = new Vector3(block2.localPosition.x, -0.5f + emptySpacePosition - 0.3f, block2.localPosition.z);
+
+                // block1.gameObject.SetActive(false);
+                // block2.gameObject.SetActive(false);
+                // block3.gameObject.SetActive(false);
             }
 
 
@@ -51,6 +93,7 @@ namespace Flappy
         private void OnCollisionEnter(Collision collision)
         {
             Debug.Log("Collided with pillar!");
+            gameManager.GameOver();
         }
 
         private void OnTriggerExit(Collider other)
